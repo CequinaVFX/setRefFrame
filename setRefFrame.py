e0a86c9fcfb0e85435454e2aa18f161d57ff77db
Cequina <lucianocequinel@gmail.com>
2022-07-31 18:07:40 +0100

    REFACTOR: new optimized script from scratch ADD: (optional) UI to get specific frame RELEASE: v2.2.1

--- setRefFrame.py
+++ setRefFrame.py
@@ -1,42 +1,94 @@
-# created by Luciano Cequinel
-# version 2.0.0
-# realease date 12/16/2021
-
+#******************************************************
+# content: simple script to quickly update reference frame on a selected list of nodes
+# if the option to use current frame, or to write a specific one
+#
+# version: 2.2.1
+# date: July 31 2022
+#
+# how to: getFrameUI()
+# dependencies: nuke
+# todos: --//--
+#
+# license: MIT
+# author: Luciano Cequinel [lucianocequinel@gmail.com]
+#******************************************************
 
 import nuke
 
-def changeFrame():
+##################
+# global variables
 
-    supportedNodes = ['Tracker4', 'FrameHold', 'Roto', 'Transform', 'CornerPin2D']
+SUPPORTED_NODES = ['Tracker4', 'FrameHold', 'Roto', 'RotoPaint', 'Transform', 'CornerPin2D', 'VectorDistort']
+SUPPORTED_KNOBS = ['reference_frame', 'referenceFrame', 'first_frame', 'ref_frame']
+
+
+def changeFrame(newFrame, node):
    
-    selNode = nuke.selectedNodes()
+    for knob in node.knobs():
+        if knob in SUPPORTED_KNOBS:
+            node[knob].setValue(int(newFrame))
+            print ('{}.{} got the value: {}'.format (node.name(), (knob), newFrame))
 
-    if len (selNode) == 0:
-        nuke.message('Select something!')
+
+def getCurrentFrame():
+    '''
+    use the current frame from current viewer
+    '''
+    newFrame = nuke.frame()
+    selNodes = nuke.selectedNodes()
+
+    if len (selNodes) > 0:
+        for node in selNodes:
+            if node.Class() in SUPPORTED_NODES:
+                changeFrame(newFrame, node)
 
     else:
-        for node in selNode:
-            if node.Class() in supportedNodes:
-
-                if node.knob('reference_frame'):
-                    node['reference_frame'].setValue(nuke.frame())
-                    print ('%s.reference_frame new value: %s' % (node.name(), (nuke.frame())))
-
-                elif node.knob('first_frame'):
-                    node['first_frame'].setValue(nuke.frame())
-                    print ('%s.first_frame new value: %s' % (node.name(), (nuke.frame())))
-
-                elif node.knob('ref_frame'):
-                    node['ref_frame'].setValue(nuke.frame())
-                    print ('%s.ref_frame new value: %s' % (node.name(), (nuke.frame())))
+        nuke.message('Select some valid node\n{}!'.format (SUPPORTED_NODES))
+        return
 
 
-            else:
-                print ("%s doesn't have a valid knob:" % (node.name()))
+def getFrameUI():
+    '''
+    get a specific frame from user
+    '''
+
+    selNodes = nuke.selectedNodes()
+
+    validNodes = []
+
+    for node in selNodes:
+        if node.Class() in SUPPORTED_NODES:
+            validNodes.append(node)
+
+    if len (validNodes) > 0:
+
+        newFrame = (nuke.getInput('set frame to', '%s' % str(nuke.frame())))
+
+        if newFrame == None:
+            return 
+
+        elif newFrame.isdigit():
+            for node in validNodes:
+                changeFrame(newFrame, node)
+        else:
+            nuke.message('You must write an integer value! (e.g 1055)')
+            getFrameUI()
+
+    else:
+        nuke.message('Select some valid node\n{}!'.format (SUPPORTED_NODES))
+        return
 
 
 
+##########################
+# main function
+if __name__ == '__main__':
+    getFrameUI()
+
+
+#################################
 #Add a menu and assign a shortcut
 toolbar = nuke.menu('Nodes')
 cqnTools = toolbar.addMenu('CQNTools', 'Modify.png')
-cqnTools.addCommand('Change reference frame', 'setRefFrame.changeFrame()', 'crtl + shift + alt + f', icon = 'TimeOffset.png')
+cqnTools.addCommand('Set to specific frame', 'setRefFrame.changeFrame()', 'crtl + shift + alt + g', icon = 'TimeOffset.png')
+cqnTools.addCommand('Set to this frame', 'setRefFrame.changeFrame()', 'crtl + shift + alt + f', icon = 'TimeOffset.png')
